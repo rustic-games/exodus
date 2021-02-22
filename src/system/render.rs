6abb44f0969db3218eddbe279278d566f0ad320a
Jean Mertz <git@jeanmertz.com>
2021-02-22 20:12:51 +0100

    enhancement(performance): 5x faster map generation
    Signed-off-by: Jean Mertz <git@jeanmertz.com>

--- src/system/render.rs
+++ src/system/render.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
+use bevy::tasks::prelude::*;
 use bevy_rng::*;
+use parking_lot::Mutex;
+use std::sync::Arc;
 
 use crate::kind::{Position, TileSetAtlas, TileSprite};
 use crate::state::TileSet;
@@ -23,17 +26,23 @@ pub(crate) fn update_translation(
 
 #[tracing::instrument(skip(commands, tiles))]
 pub(crate) fn tiles(
-    commands: &mut Commands,
+    commands: Arc<Mutex<Commands>>,
+    pool: Res<ComputeTaskPool>,
     atlas: Res<TileSetAtlas>,
     tileset: Res<TileSet>,
-    mut tiles: Query<(Entity, &mut Tile), Changed<Tile>>,
+    tiles: Query<Entity, Changed<Tile>>,
 
     // TODO: remove, see note below
-    mut rng: Local<Rng>,
+    rng: Local<Rng>,
 ) {
     let tile_size = atlas.sprite_size as i32;
 
-    for (entity, mut tile) in tiles.iter_mut() {
+    let rng = Arc::new(Mutex::new(rng.clone()));
+
+    tiles.par_iter(50).for_each(&pool, |entity| {
+        let commands = Arc::clone(&commands);
+        let rng = Arc::clone(&rng);
+
         let tileset_tile = match tileset.tile(entity) {
             Some(tile) => tile,
             None => {
@@ -41,8 +50,6 @@ pub(crate) fn tiles(
                 return;
             }
         };
-
-        commands.set_current_entity(entity);
 
         // TODO:
         //
@@ -60,7 +67,7 @@ pub(crate) fn tiles(
         // For simplicity, right now we treat each tile as having either a tree
         // or a ground but at some point this will have to be refactored.
 
-        let is_tree = rng.gen_ratio(1, 20);
+        let is_tree = rng.lock().gen_ratio(1, 20);
         let (index, color) = if is_tree {
             (TileSprite::Tree.index(), Color::GREEN)
         } else {
@@ -68,23 +75,26 @@ pub(crate) fn tiles(
         };
 
         // TODO: fog of war
-        tile.revealed(true);
-        tile.accessible(!is_tree);
+        // tile.revealed(true);
+        // tile.accessible(!is_tree);
 
         let size = (tileset.tiles.len() / 2) as i32;
 
         let x = tileset_tile.position.x * tile_size - size * tile_size;
         let y = tileset_tile.position.y * tile_size - size * tile_size;
 
-        commands.with_bundle(SpriteSheetBundle {
-            sprite: TextureAtlasSprite { index, color },
-            texture_atlas: atlas.clone(),
-            transform: Transform {
-                translation: Vec3::new(x as f32, y as f32, 0.),
-                scale: Vec3::one(),
+        commands.lock().insert(
+            entity,
+            SpriteSheetBundle {
+                sprite: TextureAtlasSprite { index, color },
+                texture_atlas: atlas.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x as f32, y as f32, 0.),
+                    scale: Vec3::one(),
+                    ..Default::default()
+                },
                 ..Default::default()
             },
-            ..Default::default()
-        });
-    }
+        );
+    })
 }
